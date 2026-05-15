5f43093418c1f47dc440e852fe544f854797f295
Shibu Sharma <168130059+shibusharma1@users.noreply.github.com>
2026-05-15 12:51:00 +0545

    index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tailwind PHP</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+
+    <div class="bg-white p-6 rounded-xl shadow-lg">
+        <h1 class="text-2xl font-bold text-blue-600">
+            Hello Tailwind in PHP 🚀
+        </h1>
+    </div>
+
+</body>
+</html>
